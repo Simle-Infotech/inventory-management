5b488f2024b2542100ca71dddf99f0503b71308c
anupadkh <alwaysgetwithanup@gmail.com>
2021-01-25 13:12:08 +0545

    settings

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qyu(9l9v%^+r(vt#ecf+36#lis516#3bo5@bo-rd*d%a=!%8#!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inventory.jalpamf.com']
+ALLOWED_HOSTS = ['inventory.jalpamf.com','inventory.simle.com.np', 'inventory.sip.com.np']
 
 
 # Application definition
