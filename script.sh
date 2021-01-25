dfcbc6408d7b858e731937ae925e465486f20ad9
anupadkh <alwaysgetwithanup@gmail.com>
2021-01-25 08:46:59 +0545

    script added

---
+++ script.sh
@@ -0,0 +1,8 @@
+python manage.py makemigrations homepage
+python manage.py migrate homepage
+python manage.py makemigrations inventory
+python manage.py migrate inventory
+python manage.py makemigrations transactions
+python manage.py migrate transactions
+python manage.py makemigrations
+python manage.py migrate
